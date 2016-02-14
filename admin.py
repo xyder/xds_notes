46a16a94ed74f4c4a9ff082638f5a24dc28a0614
xyder <xydr2311@gmail.com>
2016-02-14 21:52:32 +0200

    Added models for task lists functionality.

--- admin.py
+++ admin.py
@@ -1,6 +1,6 @@
 from core.lib import create_admin_view
 from xds_notes import models
 
-create_admin_view(models.Note)
-create_admin_view(models.NotesStream)
-create_admin_view(models.NoteFile)
+create_admin_view(models.TaskProject)
+create_admin_view(models.TaskList)
+create_admin_view(models.Task)
--- models.py
+++ models.py
@@ -1,4 +1,5 @@
-from sqlalchemy import Column, Integer
+from sqlalchemy import Column, Integer, Text, ForeignKey, Enum
+from sqlalchemy.orm import relationship
 from core.database import Base
 from core.lib import get_custom_prefixer
 from xds_notes import APP_NAME
@@ -6,16 +7,48 @@ from xds_notes import APP_NAME
 prefixer = get_custom_prefixer(APP_NAME)
 
 
-class NotesStream(Base):
-    __tablename__ = prefixer('notes_streams')
+class TaskProject(Base):
+    __tablename__ = prefixer('task_projects')
     id = Column(Integer, primary_key=True)
 
+    name = Column(Text)
 
-class Note(Base):
-    __tablename__ = prefixer('notes')
+    def __repr__(self):
+        return self.name
+
+
+class TaskList(Base):
+    __tablename__ = prefixer('task_lists')
     id = Column(Integer, primary_key=True)
 
+    title = Column(Text)
 
-class NoteFile(Base):
-    __tablename__ = prefixer('note_files')
+    position = Column(Integer)
+
+    parent_id = Column(Integer, ForeignKey(prefixer('task_projects.id')), index=True)
+    parent = relationship('TaskProject', backref='task_lists')
+
+    def __repr__(self):
+        return self.title
+
+
+class Task(Base):
+    __tablename__ = prefixer('task')
     id = Column(Integer, primary_key=True)
+
+    title = Column(Text)
+    description = Column(Text)
+    state = Column(Enum('open', 'in_progress', 'closed', name='task_states'), nullable=False)
+
+    position = Column(Integer)
+
+    parent_id = Column(Integer, ForeignKey(prefixer('task_lists.id')), index=True)
+    parent = relationship('TaskList', backref='tasks')
+
+    def __repr__(self):
+        return self.title
+
+
+# register recounting listeners for these models
+Base.register_recountable_model(TaskList)
+Base.register_recountable_model(Task)
